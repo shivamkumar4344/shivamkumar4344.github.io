ef7d5c86db38a562858d5a9ee9ca598524283448
shivamkumar4344 <ydvshivam10@gmail.com>
2025-06-17 10:24:30 +0530

    styling updated

--- A2/script.js
+++ A2/script.js
@@ -1,19 +1,22 @@
 const loginForm = () => {
-    const str = `<h3>Login Form</h3>
+    const str = `<div>
+    <h3>Login Form</h3>
     <button onclick='showHome()'>Show Home</button>
     <br><br>
     <button onclick = 'RegisterForm()'>Create Account</button>`;
-    root.innerHTML = str;
+    root.innerHTML = str + "</div>";
 }
 
 const RegisterForm = () => {
-    const str = `<h3>Register Form Form</h3>
+    const str = `<div>
+    <h3>Register Form Form</h3>
     <button onclick='loginForm()'>Already a member</button>`;
-    root.innerHTML = str;
+    root.innerHTML = str+ "</div>";
 }
 
 const showHome =() =>{
-    const str = `<h2>Welcome</h2>
+    const str = `<div>
+    <h2>Welcome</h2>
     <button onclick='loginForm()'>Logout</button>`
-    root.innerHTML = str;
+    root.innerHTML = str + "</div>";
 } 
