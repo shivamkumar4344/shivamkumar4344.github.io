a7d420779c50cde79a47e2469668aa743abb1884
shivamkumar4344 <ydvshivam10@gmail.com>
2025-06-23 09:29:39 +0530

    map concpet

--- JAVASCRIPT/file9.js
+++ JAVASCRIPT/file9.js
@@ -1,24 +1,36 @@
-//array methods
-const points = [2, 5, 3, 2, 1, 8];
-const score = [10, 20, 50, 20];
-console.log(score);
-console.log(score[0]);
-score.push(70);
-console.log(score);
-console.log(score.length);
-for (let i = 0; i < score.length; i++) {
-  console.log(score[i]);
-}
-// points.forEach((value) => {
-//   console.log(value);
+// //array methods
+// const points = [2, 5, 3, 2, 1, 8];
+// const score = [10, 20, 50, 20];
+// console.log(score);
+// console.log(score[0]);
+// score.push(70);
+// console.log(score);
+// console.log(score.length);
+// for (let i = 0; i < score.length; i++) {
+//   console.log(score[i]);
+// }
+// // points.forEach((value) => {
+// //   console.log(value);
+// // });
+// // points.forEach((value, index) => {
+// //   console.log(value,index);
+// // });
+// // points.forEach((value, index, arr) => {
+// //   console.log(value, index, arr);
+// // });
+// points.forEach((a, b, c) => {
+// //   console.log(c[b]);
+//   console.log(a);
 // });
-// points.forEach((value, index) => {
-//   console.log(value,index);
-// });
-// points.forEach((value, index, arr) => {
-//   console.log(value, index, arr);
-// });
-points.forEach((a, b, c) => {
-//   console.log(c[b]);
-  console.log(a);
+
+const products = [2, 5, 3, 2, 1, 8];
+
+// products.forEach((element,index,arr)=>{
+//   console.log(element," ",index," ",arr);
+// })
+
+const newArr = products.map((val,idx)=>{
+  return val = val+5;
 });
+
+console.log(newArr);
