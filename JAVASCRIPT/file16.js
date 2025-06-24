ecff4b4126682b7476077df65ac18afbce1693d7
shivamkumar4344 <ydvshivam10@gmail.com>
2025-06-24 11:46:18 +0530

    updated

--- JAVASCRIPT/file16.js
+++ JAVASCRIPT/file16.js
@@ -3,6 +3,7 @@
 //     setTimeout(()=>{
 //         return 5;
 //     },1000);
+    // return 5;
     
 // }
 
@@ -10,12 +11,44 @@
 //     console.log(x+6);
 // }
 
-// const n = f1();
+// const n = f1(); 5
 // f2(n);
 
 
-const f1 = () =>{
+const f1 = (a) =>{
     return new Promise((resolve,reject)=>{
-        resolve(5);
+       if(a < 0) reject("Invalid Input");
+       else resolve(a);
     });
 };
+
+const f2 = (x)=>{
+    console.log(x+6);
+}
+
+f1(1)
+.then((n)=>f2(n))
+.catch((err)=>console.log(err));
+
+
+// https://jsonplaceholder.typicode.com/users
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(res=>res.json())
+// .then((data)=>{
+//     // console.log(data[0].name);
+//     data.forEach(val => {
+//         console.log(val.name," ",val.address.geo.street);
+//     });
+// })
+// .catch((err)=>console.log(err));
+
+const fetchData = async ()=>{
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    data.forEach(val => {
+        console.log(val.name);
+    });
+}
+
+fetchData();
