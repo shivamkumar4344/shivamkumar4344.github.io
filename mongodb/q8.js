adc09cb13ce9cae9c25ef113665c7ce508b29b06
shivamkumar4344 <ydvshivam10@gmail.com>
2025-06-27 11:54:29 +0530

    updated

---
+++ mongodb/q8.js
@@ -0,0 +1,35 @@
+//db.posts.insertOne( {_id:"p1",post:"Post 1" } );
+//db.posts.insertOne( {_id:"p2",post:"Post 2" } );
+//db.comments.insertOne({_id:"c1",pid:"p1",comment:"This is comment 1 of post 1"});
+//db.comments.insertOne({_id:"c2",pid:"p1",comment:"This is comment 2 of post 1"});
+//db.comments.insertOne({_id:"c3",pid:"p2",comment:"This is comment 1 of post 2"});
+//db.comments.insertOne({_id:"c4",pid:"p2",comment:"This is comment 2 of post 2"});
+//db.comments.aggregate({$project:{_id:1,comment:1}});
+//db.comments.insertOne({_id:"c5",pid:"p2",comment:"This is comment 3 of post 2"});
+//db.posts.aggregate( [ {
+// $lookup:{
+// from:"comments",
+// localField:"_id",
+// foreignField:"pid",
+// as:"commnt"
+// } } ]  );
+
+// db.posts.aggregate([
+//   {
+// 	$lookup: {
+// 	  from: "comments",
+// 	  localField: "_id",
+// 	  foreignField: "pid",
+// 	  as: "commnt"
+// 	}
+//   },
+//   {
+// 	$unwind: "$commnt"
+//   }
+// ]);
+
+// **Important**
+
+//db.marks.aggregate( {$group :{_id:"$name" , total:{$sum:"$score"} }});  -> this will directly group by name and give sum of score as total.
+//db.marks.aggregate( {$group :{_id:{subject:"$subject",name:"$name"} , totalBySubject:{$sum:"$score"} }});
+
