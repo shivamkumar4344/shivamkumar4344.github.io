296115e0270dbc39a95c6506085a2f66a325299f
shivamkumar4344 <ydvshivam10@gmail.com>
2025-06-30 11:57:46 +0530

    updated

--- mongodb/q10.js
+++ mongodb/q10.js
@@ -7,3 +7,21 @@
 // mongod -replSet rs1 -logpath "d:\mongo-replica\data2\2.log" --dbpath "d:\mongo-replica\data2" --port 27019
 
 // mongod -replSet rs1 -logpath "d:\mongo-replica\data3\3.log" --dbpath "d:\mongo-replica\data3" --port 27020
+
+
+//mongosh --port 27018
+
+// rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
+
+// rs.config()  //to check the config
+// rs.status()
+
+// Use mongosh command with the following connection string and the primary server will automatically get connected:
+
+
+// mongosh "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=rs1"
+
+// show dbs
+// use mytestdb
+// db.createCollection("customers")
+// db.customers.insertOne({name:"John"})
